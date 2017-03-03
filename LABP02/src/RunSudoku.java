fc90fd54b6a5a5dc6c22a4ba568aabe023a37d1f
Rodrigo Miguel Trole Reis <fc41941@alunos.fc.ul.pt>
2017-03-03 15:27:14 +0000

    Tests the matrix for repeated digits in columns

--- LABP02/src/RunSudoku.java
+++ LABP02/src/RunSudoku.java
@@ -12,6 +12,9 @@ public class RunSudoku {
 			int [][] myMatrix = Sudoku.fileToMatrix(args[0]);
 			Sudoku.printMatrix(myMatrix);
 			System.out.println("Has repeated lines: " + Sudoku.hasRepeatedNumbersLine(myMatrix));
+			System.out.println("Has repeated columns: " + Sudoku.hasRepeatedNumbersCols(myMatrix));
+
+			
 		}
 		
 	}
